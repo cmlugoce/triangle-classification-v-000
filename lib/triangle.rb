52338759d2dcc58ea970fa8dafee9a94a5ccedd0
Cristina Lugo <cmlugoce@syr.edu>
2018-06-17 21:16:36 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,23 +7,23 @@ def initialize(a, b, c)
   @c = c
 end 
 
-def kind 
-  if a == b && a == c
-    :equilateral 
-  elsif a == b || a == c || b == c 
-    :isosceles
-  else a != b && b != c && c != a
-    :scalene 
-  end
-  
-  if a <= 0 || b <= 0 || c <= 0
+def kind
+    if a <= 0 || b <= 0 || c <= 0
       raise TriangleError
     end
 
-    if a + b <= c || a + c <= b || c + b <= a
+    if a+b <= c || a+c <= b || b+c <= a
       raise TriangleError
-    end 
-  end 
+    end
+
+  if a == b && b == c && a == c
+    :equilateral
+  elsif a != b && b != c && c != a
+    :scalene
+  else
+    :isosceles
+  end
+end
   
   
   
